87fe12bb3e7a2d1b5a9325b428bb35f44d216b12
Jose Roberto Diaz Rodriguez <diazrodriguez.jr@gmail.com>
2020-05-05 19:37:35 -0500

    avance

--- FirebaseWeb/public/js/post/post.js
+++ FirebaseWeb/public/js/post/post.js
@@ -1,124 +1,139 @@
 class Post {
-  constructor () {
-      // TODO inicializar firestore y settings
+	constructor() {
+		this.db = firebase.firestore();
+		const settings = { timestampsInSnapshots: true };
+		this.db.settings(settings);
+	}
 
-  }
+	crearPost(uid, emailUser, titulo, descripcion, imagenLink, videoLink) {
+		return this.db
+			.collection("posts")
+			.add({
+				uid: uid,
+				autor: emailUser,
+				titulo: titulo,
+				descripcion: descripcion,
+				imagenLink: imagenLink,
+				videoLink: videoLink,
+				fecha: firebase.firestore.FieldValue.serverTimestamp(),
+			})
+			.then((refDoc) => {
+				console.log(`Id del post => ${refDoc.id}`);
+			})
+			.catch((error) => {
+				console.error(`Error creando el post => ${error}`);
+			});
+	}
 
-  crearPost (uid, emailUser, titulo, descripcion, imagenLink, videoLink) {
-    
-  }
+	consultarTodosPost() {}
 
-  consultarTodosPost () {
-    
-  }
+	consultarPostxUsuario(emailUser) {}
 
-  consultarPostxUsuario (emailUser) {
-    
-  }
+	subirImagenPost(file, uid) {}
 
-  obtenerTemplatePostVacio () {
-    return `<article class="post">
-      <div class="post-titulo">
-          <h5>Crea el primer Post a la comunidad</h5>
-      </div>
-      <div class="post-calificacion">
-          <a class="post-estrellita-llena" href="*"></a>
-          <a class="post-estrellita-llena" href="*"></a>
-          <a class="post-estrellita-llena" href="*"></a>
-          <a class="post-estrellita-llena" href="*"></a>
-          <a class="post-estrellita-vacia" href="*"></a>
-      </div>
-      <div class="post-video">
-          <iframe type="text/html" width="500" height="385" src='https://www.youtube.com/embed/bTSWzddyL7E?ecver=2'
-              frameborder="0"></iframe>
-          </figure>
-      </div>
-      <div class="post-videolink">
-          Video
-      </div>
-      <div class="post-descripcion">
-          <p>Crea el primer Post a la comunidad</p>
-      </div>
-      <div class="post-footer container">         
-      </div>
-  </article>`
-  }
+	obtenerTemplatePostVacio() {
+		return `<article class="post">
+        <div class="post-titulo">
+            <h5>Crea el primer Post a la comunidad</h5>
+        </div>
+        <div class="post-calificacion">
+            <a class="post-estrellita-llena" href="*"></a>
+            <a class="post-estrellita-llena" href="*"></a>
+            <a class="post-estrellita-llena" href="*"></a>
+            <a class="post-estrellita-llena" href="*"></a>
+            <a class="post-estrellita-vacia" href="*"></a>
+        </div>
+        <div class="post-video">
+            <iframe type="text/html" width="500" height="385" src='https://www.youtube.com/embed/bTSWzddyL7E?ecver=2'
+                frameborder="0"></iframe>
+            </figure>
+        </div>
+        <div class="post-videolink">
+            Video
+        </div>
+        <div class="post-descripcion">
+            <p>Crea el primer Post a la comunidad</p>
+        </div>
+        <div class="post-footer container">         
+        </div>
+    </article>`;
+	}
 
-  obtenerPostTemplate (
-    autor,
-    titulo,
-    descripcion,
-    videoLink,
-    imagenLink,
-    fecha
-  ) {
-    if (imagenLink) {
-      return `<article class="post">
-            <div class="post-titulo">
-                <h5>${titulo}</h5>
-            </div>
-            <div class="post-calificacion">
-                <a class="post-estrellita-llena" href="*"></a>
-                <a class="post-estrellita-llena" href="*"></a>
-                <a class="post-estrellita-llena" href="*"></a>
-                <a class="post-estrellita-llena" href="*"></a>
-                <a class="post-estrellita-vacia" href="*"></a>
-            </div>
-            <div class="post-video">                
-                <img id="imgVideo" src='${imagenLink}' class="post-imagen-video" 
-                    alt="Imagen Video">     
-            </div>
-            <div class="post-videolink">
-                <a href="${videoLink}" target="blank">Ver Video</a>                            
-            </div>
-            <div class="post-descripcion">
-                <p>${descripcion}</p>
-            </div>
-            <div class="post-footer container">
-                <div class="row">
-                    <div class="col m6">
-                        Fecha: ${fecha}
-                    </div>
-                    <div class="col m6">
-                        Autor: ${autor}
-                    </div>        
-                </div>
-            </div>
-        </article>`
-    }
+	obtenerPostTemplate(
+		autor,
+		titulo,
+		descripcion,
+		videoLink,
+		imagenLink,
+		fecha
+	) {
+		if (imagenLink) {
+			return `<article class="post">
+              <div class="post-titulo">
+                  <h5>${titulo}</h5>
+              </div>
+              <div class="post-calificacion">
+                  <a class="post-estrellita-llena" href="*"></a>
+                  <a class="post-estrellita-llena" href="*"></a>
+                  <a class="post-estrellita-llena" href="*"></a>
+                  <a class="post-estrellita-llena" href="*"></a>
+                  <a class="post-estrellita-vacia" href="*"></a>
+              </div>
+              <div class="post-video">                
+                  <img id="imgVideo" src='${imagenLink}' class="post-imagen-video" 
+                      alt="Imagen Video">     
+              </div>
+              <div class="post-videolink">
+                  <a href="${videoLink}" target="blank">Ver Video</a>                            
+              </div>
+              <div class="post-descripcion">
+                  <p>${descripcion}</p>
+              </div>
+              <div class="post-footer container">
+                  <div class="row">
+                      <div class="col m6">
+                          Fecha: ${fecha}
+                      </div>
+                      <div class="col m6">
+                          Autor: ${autor}
+                      </div>        
+                  </div>
+              </div>
+          </article>`;
+		}
 
-    return `<article class="post">
-                <div class="post-titulo">
-                    <h5>${titulo}</h5>
-                </div>
-                <div class="post-calificacion">
-                    <a class="post-estrellita-llena" href="*"></a>
-                    <a class="post-estrellita-llena" href="*"></a>
-                    <a class="post-estrellita-llena" href="*"></a>
-                    <a class="post-estrellita-llena" href="*"></a>
-                    <a class="post-estrellita-vacia" href="*"></a>
-                </div>
-                <div class="post-video">
-                    <iframe type="text/html" width="500" height="385" src='${videoLink}'
-                        frameborder="0"></iframe>
-                    </figure>
-                </div>
-                <div class="post-videolink">
-                    Video
-                </div>
-                <div class="post-descripcion">
-                    <p>${descripcion}</p>
-                </div>
-                <div class="post-footer container">
-                    <div class="row">
-                        <div class="col m6">
-                            Fecha: ${fecha}
-                        </div>
-                        <div class="col m6">
-                            Autor: ${autor}
-                        </div>        
-                    </div>
-                </div>
-            </article>`
-  }
+		return `<article class="post">
+                  <div class="post-titulo">
+                      <h5>${titulo}</h5>
+                  </div>
+                  <div class="post-calificacion">
+                      <a class="post-estrellita-llena" href="*"></a>
+                      <a class="post-estrellita-llena" href="*"></a>
+                      <a class="post-estrellita-llena" href="*"></a>
+                      <a class="post-estrellita-llena" href="*"></a>
+                      <a class="post-estrellita-vacia" href="*"></a>
+                  </div>
+                  <div class="post-video">
+                      <iframe type="text/html" width="500" height="385" src='${videoLink}'
+                          frameborder="0"></iframe>
+                      </figure>
+                  </div>
+                  <div class="post-videolink">
+                      Video
+                  </div>
+                  <div class="post-descripcion">
+                      <p>${descripcion}</p>
+                  </div>
+                  <div class="post-footer container">
+                      <div class="row">
+                          <div class="col m6">
+                              Fecha: ${fecha}
+                          </div>
+                          <div class="col m6">
+                              Autor: ${autor}
+                          </div>        
+                      </div>
+                  </div>
+              </article>`;
+	}
 }
